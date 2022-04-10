af05802bcc57908b8f940ccfb8202871243f0770
aliviaankrum <82051538+aliviaankrum@users.noreply.github.com>
2022-04-10 01:35:09 -0500

    Update run.sql

--- run.sql
+++ run.sql
@@ -1,10 +1,1 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\csgray\c0;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 \
-LOAD DATA LOCAL INFILE \'91./
-\f1\fs22 \cf2 \CocoaLigature0 organisms_ERR1072624.csv\'92 INTO TABLE organisms FIELDS TERMINATED BY \'91,\'92 FIELDS ENCLOSED BY \'91\\\'92\'92 LINES TERMINATED BY \'91\\n\'92; }
+LOAD DATA LOCAL INFILE ‘./organisms_ERR1072624.csv’ INTO TABLE organisms FIELDS TERMINATED BY ‘,’ FIELDS ENCLOSED BY ‘\’’ LINES TERMINATED BY ‘\n’; 
